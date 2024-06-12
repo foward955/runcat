08a131e37c918d122b1495b5c4ea3fac864997f3
is-dw <ldw66666520@gmail.com>
2024-06-12 20:02:03 +0800

    feat: cat run

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 
 use std::sync::Arc;
 
+use crossbeam_channel::{Receiver, Sender};
 use once_cell::sync::Lazy;
 use parking_lot::Mutex;
+use sysinfo::System;
 use tray_icon::{
     menu::{IsMenuItem, Menu, MenuEvent, MenuEventReceiver, MenuId, MenuItem},
-    TrayIcon, TrayIconBuilder, TrayIconEvent, TrayIconEventReceiver,
+    Icon, TrayIcon, TrayIconBuilder, TrayIconEvent, TrayIconEventReceiver,
 };
 use winit::{
     application::ApplicationHandler,
@@ -16,6 +18,8 @@ use winit::{
 
 static EVENT_LOOP_PROXY: Lazy<Arc<Mutex<Option<EventLoopProxy<RunCatTrayEvent>>>>> =
     Lazy::new(|| Arc::new(Mutex::new(None)));
+
+const MAX_CAT_INDEX: usize = 4;
 
 pub(crate) struct RunCatTray {
     tray_menu: Menu,
@@ -27,12 +31,61 @@ pub(crate) struct RunCatTray {
     tary_icon: Option<TrayIcon>,
     curr_theme: dark_light::Mode,
     auto_fit_theme: bool,
+
+    resource: Cat,
 }
 
 #[derive(Debug)]
 pub(crate) enum RunCatTrayEvent {
     TrayMenuEvent(MenuEvent),
     SystemThemeChanged(dark_light::Mode),
+    TrayIconEvent(usize),
+}
+
+pub struct Cat {
+    pub dark: Vec<Icon>,
+    pub light: Vec<Icon>,
+}
+
+const CATS_LIGHT: [&str; 5] = [
+    "/src/cat/light_cat_0.ico",
+    "/src/cat/light_cat_1.ico",
+    "/src/cat/light_cat_2.ico",
+    "/src/cat/light_cat_3.ico",
+    "/src/cat/light_cat_4.ico",
+];
+
+const CATS_DARK: [&str; 5] = [
+    "/src/cat/dark_cat_0.ico",
+    "/src/cat/dark_cat_1.ico",
+    "/src/cat/dark_cat_2.ico",
+    "/src/cat/dark_cat_3.ico",
+    "/src/cat/dark_cat_4.ico",
+];
+
+impl Cat {
+    fn load() -> Cat {
+        Cat {
+            light: CATS_LIGHT
+                .map(|f| {
+                    load_icon(std::path::Path::new(&format!(
+                        "{}{}",
+                        env!("CARGO_MANIFEST_DIR"),
+                        f
+                    )))
+                })
+                .to_vec(),
+            dark: CATS_DARK
+                .map(|f| {
+                    load_icon(std::path::Path::new(&format!(
+                        "{}{}",
+                        env!("CARGO_MANIFEST_DIR"),
+                        f
+                    )))
+                })
+                .to_vec(),
+        }
+    }
 }
 
 impl RunCatTray {
@@ -40,6 +93,8 @@ impl RunCatTray {
         let auto_fit_theme = MenuItem::new("auto fit theme: true", true, None);
         let toggle_theme = MenuItem::new("toggle theme", false, None);
         let exit = MenuItem::new("exit", true, None);
+
+        let (cpu_tx, cpu_rx) = crossbeam_channel::unbounded::<f32>();
 
         Self {
             tray_menu: Menu::with_items(&[&auto_fit_theme, &toggle_theme, &exit]).unwrap(),
@@ -52,12 +107,57 @@ impl RunCatTray {
 
             curr_theme: dark_light::detect(),
             auto_fit_theme: true,
+
+            resource: Cat::load(),
         }
+    }
+
+    fn moditor_cpu_usage(&mut self) {
+        let mut sys = System::new();
+        let mut usage_cache = 1.0;
+        let (cpu_tx, cpu_rx) = crossbeam_channel::unbounded();
+
+        let mut i = 0;
+
+        // cpu stats calculation thread
+        std::thread::spawn(move || loop {
+            std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+            sys.refresh_cpu_usage();
+            let cpu_usage = sys.global_cpu_info().cpu_usage();
+
+            cpu_tx.send(cpu_usage).unwrap();
+        });
+
+        std::thread::spawn(move || loop {
+            let cpu_usage = if let Ok(usage) = cpu_rx.try_recv() {
+                usage_cache = usage;
+                usage
+            } else {
+                usage_cache
+            };
+
+            let cmp_f = [20.0, cpu_usage / 5.0];
+            let min = cmp_f.iter().fold(f32::NAN, |m, v| v.min(m));
+            let cmp_f = [1.0, min];
+            let max = cmp_f.iter().fold(f32::NAN, |m, v| v.max(m));
+            std::thread::sleep(std::time::Duration::from_millis((200.0 / max) as u64));
+            i += 1;
+
+            if i > MAX_CAT_INDEX {
+                i = 0;
+            }
+
+            if let Some(proxy) = EVENT_LOOP_PROXY.lock().as_ref() {
+                proxy.send_event(RunCatTrayEvent::TrayIconEvent(i)).unwrap();
+            }
+        });
     }
 
     // fn add_menu_item(&mut self, item: MenuItem, handler: fn(e: MenuEvent)) {
     //     self.menu_items.push((item, handler));
     // }
+
+    fn change_tray_icon(&self, i: usize) {}
 
     fn on_theme_changed(&mut self) {
         if let Some(tray_icon) = self.tary_icon.as_mut() {
@@ -66,7 +166,6 @@ impl RunCatTray {
             } else {
                 load_icon(std::path::Path::new(self.icon_path.1))
             };
-            println!("ddd: {:?}", self.curr_theme);
             tray_icon.set_icon(Some(icon)).unwrap();
         }
     }
@@ -100,6 +199,8 @@ impl ApplicationHandler<RunCatTrayEvent> for RunCatTray {
         );
 
         self.on_theme_changed();
+
+        self.moditor_cpu_usage();
     }
 
     fn window_event(
@@ -151,6 +252,16 @@ impl ApplicationHandler<RunCatTrayEvent> for RunCatTray {
                     self.on_theme_changed();
                 }
             }
+            RunCatTrayEvent::TrayIconEvent(i) => {
+                if let Some(tray_icon) = self.tary_icon.as_mut() {
+                    let icon = if self.curr_theme == dark_light::Mode::Dark {
+                        self.resource.dark[i].clone()
+                    } else {
+                        self.resource.light[i].clone()
+                    };
+                    tray_icon.set_icon(Some(icon)).unwrap();
+                }
+            }
         }
     }
 
@@ -180,9 +291,7 @@ fn main() {
     let event_loop = EventLoop::<RunCatTrayEvent>::with_user_event()
         .build()
         .expect("can't start the event loop");
-
     *EVENT_LOOP_PROXY.lock() = Some(event_loop.create_proxy());
-
     let mut app = RunCatTray::new(path);
 
     event_loop.run_app(&mut app).unwrap();
